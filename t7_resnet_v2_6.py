e432fafa9563a08938600e3023f4b929e6e0fdc5
Max Ferguson <maxkferg@gmail.com>
2018-10-17 13:31:31 -0700

    Print the lr

--- t7_resnet_v2_6.py
+++ t7_resnet_v2_6.py
@@ -180,9 +180,9 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 		running_corrects = 0
 		incorrect = {'00':0, '01':0, '02':0, '03':0, '10':0, '11':0, '12':0, '13':0, '20':0, '21':0, '22':0, '23':0, '30':0, '31':0, '32':0, '33':0}
 		# Update learning rate: StepLR
-		if type(scheduler) == torch.optim.lr_scheduler.StepLR: 
-			scheduler.step() 
-			print(dir(scheduler))
+		if type(scheduler) == torch.optim.lr_scheduler.StepLR:
+			scheduler.step()
+			print("Current learning rate: ", scheduler.get_lr())
 		for i, (inputs, labels) in enumerate(train_loader):
 			inputs = inputs.to(device)
 			labels = labels.to(device)
